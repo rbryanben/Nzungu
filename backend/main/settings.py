589dce089fe7c04143005dc3647892c7894ced72
rbryanben <rbryanben@gmail.com>
2025-08-27 19:43:12 +0200

    Setup Backend - Actual database

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 
+
 load_dotenv()
+
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,8 +84,15 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME"), 
+        'USER': os.getenv("DB_USER"),     
+        'PASSWORD': os.getenv("DB_PASSWORD"), 
+        'HOST': os.getenv("DB_HOST"),             
+        'PORT': os.getenv("DB_PORT"),
+        'OPTIONS': {
+            'ssl_mode': 'REQUIRED'
+        }
     }
 }
 
