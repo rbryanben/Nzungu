0eb71c9cedd472c03c2e1322484389cd0f119ccd
rbryanben <rbryanben@gmail.com>
2025-08-28 10:32:14 +0200

    Django admin site static fix

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -131,20 +131,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = 'https://nzungu-auxiliary.s3.af-south-1.amazonaws.com/django-admin/'
+
+STATIC_URL = 'https://nzungu-auxiliary.s3.af-south-1.amazonaws.com/django-admin/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8080',
-    'http://localhost:8080',
-    "http://192.168.0.186:8080"
-]
-
 CORS_ALLOW_ALL_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = [' https://backend.bradfeild.yanney.co.zw']
 
 CACHES = {
     "default": {
